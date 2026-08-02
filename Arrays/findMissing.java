862da662b1c03164a244a2f66b7d5a8842a7ee6f
Divya <dasdivya589@gmail.com>
2026-08-02 13:25:38 +0530

    Find the missing number

---
+++ Arrays/findMissing.java
@@ -0,0 +1,28 @@
+public class findMissing {
+    public int findMissingNum(int[] arr) {
+
+        int actualSum = 0;
+
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            actualSum += arr[i];
+        }
+
+        int expectedSum = n * (n + 1)/2;
+
+        int missingNum = expectedSum - actualSum;
+
+        return missingNum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 8};
+
+        findMissing obj = new findMissing();
+
+        int missingNum = obj.findMissingNum(arr);
+
+        System.out.println("Missing number is: " + missingNum);
+    }
+}
